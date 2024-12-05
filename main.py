4d773602dc87600d46f02ff80e7ed36b7eb35466
Лиза <elisaveta.ponkina@yandex.ru>
2024-12-05 22:21:04 +0700

    обновление

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import io
 
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow
@@ -7,12 +8,64 @@ from PyQt6.QtCore import QPointF
 from random import randint, choice
 
 
+class Design:
+    def __init__(self, ex):
+        a = """<?xml version="1.0" encoding="UTF-8"?>
+        <ui version="4.0">
+         <class>MainWindow</class>
+         <widget class="QMainWindow" name="MainWindow">
+          <property name="geometry">
+           <rect>
+            <x>0</x>
+            <y>0</y>
+            <width>602</width>
+            <height>638</height>
+           </rect>
+          </property>
+          <property name="windowTitle">
+           <string>MainWindow</string>
+          </property>
+          <widget class="QWidget" name="centralwidget">
+           <widget class="QPushButton" name="add">
+            <property name="geometry">
+             <rect>
+              <x>250</x>
+              <y>490</y>
+              <width>131</width>
+              <height>81</height>
+             </rect>
+            </property>
+            <property name="text">
+             <string>Круг</string>
+            </property>
+           </widget>
+          </widget>
+          <widget class="QMenuBar" name="menubar">
+           <property name="geometry">
+            <rect>
+             <x>0</x>
+             <y>0</y>
+             <width>602</width>
+             <height>21</height>
+            </rect>
+           </property>
+          </widget>
+          <widget class="QStatusBar" name="statusbar"/>
+         </widget>
+         <resources/>
+         <connections/>
+        </ui>"""
+
+        f = io.StringIO(a)
+        uic.loadUi(f, ex)
+
+
 class Project(QMainWindow):
     def __init__(self):
         try:
             self.f = False
             super().__init__()
-            uic.loadUi('UI.ui', self)
+            self.d = Design(self)
             self.add.clicked.connect(self.run)
         except Exception as e:
             print('%s' % e)
@@ -25,7 +78,7 @@ class Project(QMainWindow):
                 x = 300
                 y = 300
                 r = choice(range(20, 200))
-                color = QColor(255, 255, 0)
+                color = QColor(randint(0, 256), randint(0, 256), randint(0, 256))
                 qp.setBrush(color)
                 qp.drawEllipse(QPointF(float(x), float(y)), r * 2, r * 2)
                 qp.end()
@@ -36,8 +89,6 @@ class Project(QMainWindow):
     def run(self):
         self.f = True
         self.update()
-
-
 
 
 if __name__ == '__main__':
